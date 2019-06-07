b46d3463915543098714d310c632eb281dcfd07a
Nate Weaver <wevah@derailer.org>
2019-06-07 12:15:09 -0500

    Initial Commit

---
+++ dragterm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  dragterm
+//
+//  Created by Nate Weaver on 2019-06-07.
+//  Copyright © 2019 Nate Weaver/Derailer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
